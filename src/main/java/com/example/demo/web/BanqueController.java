8dbf3d3c3692fb769b4d36833c01df75188fa795
assabbour jaouad <assabbour@hotmail.com>
2019-11-14 15:04:12 +0000

    deploy heroku

--- src/main/java/com/example/demo/web/BanqueController.java
+++ src/main/java/com/example/demo/web/BanqueController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.demo.metier.IBanqueMetier;
 
@@ -39,6 +41,30 @@ public class BanqueController {
 		return "comptes";
 		
 	}
+	
+	
+	@RequestMapping(value="saveOperation", method = RequestMethod.POST)
+	public String saveOperation(Model model, String typeOperation, String codeCompte, double montant, String codeCompte2 ) {
+		
+		try {
+			if(typeOperation.equals("VERS")) {
+				banqueMetier.verser(codeCompte, montant);
+			}
+			else if(typeOperation.equals("RET")) {
+				banqueMetier.retirer(codeCompte, montant);
+			}
+			if(typeOperation.equals("VIR")){
+				banqueMetier.virement(codeCompte, codeCompte2, montant);
+				
+			}
+		} catch (Exception e) {
+			model.addAttribute("hello", e);
+		}
+		
+		return "redirect:/consultercompte?codeCompte="+codeCompte;
+	}
+	
+	
 	
 }
 
